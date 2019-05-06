3c6883815e96862aa53690d631313977c2498613
Gibsern Koh <gibsernkoh@gmail.com>
2019-05-06 12:39:54 +0800

    Updates

---
+++ precache-manifest.1f8ebe06da813f6267f0c85ee16390bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b039ef817dda5fa5787",
+    "url": "/blog/static/css/main.538444a3.chunk.css"
+  },
+  {
+    "revision": "3b039ef817dda5fa5787",
+    "url": "/blog/static/js/main.71442bf4.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "45a94f0a99030785234d",
+    "url": "/blog/static/css/2.dddd57a7.chunk.css"
+  },
+  {
+    "revision": "45a94f0a99030785234d",
+    "url": "/blog/static/js/2.36829ede.chunk.js"
+  },
+  {
+    "revision": "8e4c47f2fb2c1eb7fc3c111c13cb1406",
+    "url": "/blog/static/media/bookmark.8e4c47f2.jpg"
+  },
+  {
+    "revision": "3d8eafcdac399841d1b2a53e08061bca",
+    "url": "/blog/index.html"
+  }
+];
